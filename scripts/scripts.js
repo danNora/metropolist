f12a6109dff627ef6f667913a952420cf89dd875
Nora Zheng <zhengnuo@gmail.com>
2019-08-28 18:46:14 -0400

    API Call promises sorted

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,24 @@
 const app = {};
-app.apiDataPoints = ["images", "scores"];
+app.apiDataPoints = ['images', 'scores'];
 
 app.getCityPromise = function(cityName, dataPoint) {
 	const endpoint = `https://api.teleport.org/api/urban_areas/slug:${cityName}/${dataPoint}`;
 	// console.log(endpoint);
 	return $.ajax({
 		url: endpoint,
-		method: "GET",
-		dataType: "json"
+		method: 'GET',
+		dataType: 'json'
 	});
 };
 
-app.getCityData = async (cityName) => {
+app.getCityData = async cityName => {
 	const cityData = {};
 
 	for (let i = 0; i < app.apiDataPoints.length; i++) {
-		const cityDataPoint = await app.getCityPromise(cityName, app.apiDataPoints[i]);
+		const cityDataPoint = await app.getCityPromise(
+			cityName,
+			app.apiDataPoints[i]
+		);
 		cityData[app.apiDataPoints[i]] = cityDataPoint;
 	}
 
@@ -23,11 +26,20 @@ app.getCityData = async (cityName) => {
 };
 
 app.parseCityData = function(cityData) {
-	console.log(cityData["images"]);
-	const imageHtml = `<img src="${cityData["images"].photos[0].image.mobile} alt=""`;
-	console.log(cityData["scores"]);	
+	// console.log(cityData['images']);
+	const imageHtml = `<img src="${cityData['images'].photos[0].image.mobile} alt="Landscape of the city of city name">`;
+	const cityTeleportScore = Math.round(
+		cityData['scores']['teleport_city_score']
+	);
+
+	const cityScoreArray = cityData['scores']['categories'];
+	console.log(cityData['scores']);
 
 	// Convert to HTML
+	const scoresHtml = $('.lQItems');
+	cityScoreArray.forEach(function(object) {
+		scoresHtml.append(`<li class="lQItem">`);
+	});
 
 	// app.displayCityData(parsedData);
 };
@@ -35,16 +47,25 @@ app.parseCityData = function(cityData) {
 app.displayCityData = function(parsedCityData) {
 	// jQuery happens here
 
-	app.parseCityData()
+	app.parseCityData();
 };
-
-
 
 app.init = function() {
 	$('#submit').on('click', async function(e) {
 		e.preventDefault();
-		app.userCity1 = $('#location1').val().trim().toLowerCase().split(" ").join("-");
-		app.userCity2 = $('#location2').val().trim().toLowerCase().split(" ").join("-");
+		//todo: function
+		app.userCity1 = $('#location1')
+			.val()
+			.trim()
+			.toLowerCase()
+			.split(' ')
+			.join('-');
+		app.userCity2 = $('#location2')
+			.val()
+			.trim()
+			.toLowerCase()
+			.split(' ')
+			.join('-');
 
 		app.getCityData(app.userCity1);
 		app.getCityData(app.userCity2);
