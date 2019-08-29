3590a96a0a2f7b6ebca94c43dfb7f403a09775f7
Nora Zheng <zhengnuo@gmail.com>
2019-08-29 18:16:23 -0400

    cleaned up the code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,39 +22,36 @@ app.getCityData = async cityName => {
 		cityData[app.apiDataPoints[i]] = cityDataPoint;
 	}
 
-	console.log(cityData);
-	app.parseCityData(cityData);
+	app.displayCityData(cityData);
 };
 
-app.parseCityData = function(cityData) {
-	// console.log(cityData['/images']);
+app.displayCityData = function(cityData) {
+	//An overall score of the city given by Teleport
+	// const cityTeleportScore = cityData['/scores']['teleport_city_score'].toFixed(1);
 
-	const cityTeleportScore = cityData['/scores']['teleport_city_score'].toFixed(
-		1
-	);
+	// A div>ul to house all the <li>
+	const $result = $("<div class='result'>");
+	const $scoresList = $('<ul class="lQItems">');
+
+	// Get data out of objects
 	const cityName = cityData['/']['full_name'];
 
-	// Convert to HTML
+	// Get city image url
+	const cityImage = cityData['/images'].photos[0].image.mobile;
 
-	const $result = $("<div class='result'>");
-
+	// Create div.imgWrapper>img
 	const $imageHtml = $(
-		`<div class="imgWrapper"><img src="${cityData['/images'].photos[0].image.mobile}" alt="Landscape of the city of ${cityName}"></div>`
+		`<div class="imgWrapper"><img src="${cityImage}" alt="Landscape of the city of ${cityName}"></div>`
 	);
 
 	const $cityTitle = $(`<h2>${cityName}</h2>`);
-	const $cityDescription = $('<p>city description</p>');
-	const $scoresList = $('<ul class="lQItems">');
-	// $result.append()
+	// const $cityDescription = $('<p>city description</p>');
 
 	const cityScoreArray = cityData['/scores']['categories'];
-	// console.log(cityScoreArray);
 
-	// creating <li.lQitem
 	cityScoreArray.forEach(function(score) {
 		const $score = $('<li class="lQItem">');
 		const $itemTitle = $('<h3 class="itemTitle">').text(score.name);
-
 		const $scoreNum = $('<span> class="scoreNum">').text(
 			score['score_out_of_10'] ? score['score_out_of_10'].toFixed(1) : 'N/A'
 		);
@@ -62,22 +59,16 @@ app.parseCityData = function(cityData) {
 		const $scoreBarFull = $('<div class="scoreBar scoreBarFull">');
 		const $scoreBarFill = $('<div class="scoreBar scoreBarFill">').css({
 			width: score['score_out_of_10'] * 10 + '%',
-			height: '10px',
 			background: score['color']
 		});
 
 		$scoreBarFull.html($scoreBarFill);
 		$score.append($itemTitle, $scoreNum, $scoreBarFull);
 		$scoresList.append($score);
-		$result.append($imageHtml, $cityTitle, $scoresList);
 	});
 
+	$result.append($imageHtml, $cityTitle, $scoresList);
 	$('.results').append($result);
-};
-
-app.displayCityData = function(parsedCityData) {
-	// jQuery happens here
-	// app.parseCityData();
 };
 
 app.cleanUserInput = function(inputValue) {
