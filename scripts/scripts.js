9eeb3d36bd297066cd624baed5b5ba1f6ed0184c
NoraZheng <zhengnuo@gmail.com>
2019-08-29 00:30:06 -0400

    city score item html to be finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,78 +2,94 @@ const app = {};
 app.apiDataPoints = ['images', 'scores'];
 
 app.getCityPromise = function(cityName, dataPoint) {
-	const endpoint = `https://api.teleport.org/api/urban_areas/slug:${cityName}/${dataPoint}`;
-	// console.log(endpoint);
-	return $.ajax({
-		url: endpoint,
-		method: 'GET',
-		dataType: 'json'
-	});
+  const endpoint = `https://api.teleport.org/api/urban_areas/slug:${cityName}/${dataPoint}`;
+  // console.log(endpoint);
+  return $.ajax({
+    url: endpoint,
+    method: 'GET',
+    dataType: 'json'
+  });
 };
 
 app.getCityData = async cityName => {
-	const cityData = {};
+  const cityData = {};
 
-	for (let i = 0; i < app.apiDataPoints.length; i++) {
-		const cityDataPoint = await app.getCityPromise(
-			cityName,
-			app.apiDataPoints[i]
-		);
-		cityData[app.apiDataPoints[i]] = cityDataPoint;
-	}
+  for (let i = 0; i < app.apiDataPoints.length; i++) {
+    const cityDataPoint = await app.getCityPromise(
+      cityName,
+      app.apiDataPoints[i]
+    );
+    cityData[app.apiDataPoints[i]] = cityDataPoint;
+  }
 
-	app.parseCityData(cityData);
+  app.parseCityData(cityData);
 };
 
 app.parseCityData = function(cityData) {
-	// console.log(cityData['images']);
-	const imageHtml = `<img src="${cityData['images'].photos[0].image.mobile} alt="Landscape of the city of city name">`;
-	const cityTeleportScore = Math.round(
-		cityData['scores']['teleport_city_score']
-	);
+  // console.log(cityData['images']);
+  const imageHtml = `<img src="${cityData['images'].photos[0].image.mobile} alt="Landscape of the city of city name">`;
+  const cityTeleportScore = Math.round(
+    cityData['scores']['teleport_city_score']
+  );
 
-	const cityScoreArray = cityData['scores']['categories'];
-	console.log(cityData['scores']);
+  const cityScoreArray = cityData['scores']['categories'];
+  console.log(cityScoreArray);
 
-	// Convert to HTML
-	const scoresHtml = $('.lQItems');
-	cityScoreArray.forEach(function(object) {
-		scoresHtml.append(`<li class="lQItem">`);
-	});
+  // Convert to HTML
+  const scoresHtml = $('.lQItems');
 
-	// app.displayCityData(parsedData);
+  cityScoreArray.forEach(function(object) {
+    const itemTitle = `<h3 class="itemTitle">${object.name}</h3>`;
+    const scoreNum = `<span class="scoreNum">${Math.round(
+      object['score_out_of_10']
+    )}</span>`;
+    const scoreBarFill = `<div class='scoreBar scoreBarFill'></div>`;
+
+    const scoreBarFull = `<div class="scoreBar scoreBarFull"></div>`;
+
+    const scoreItem = `<li class="lQItem"></li>`;
+
+    // app.displayCityData(parsedData);
+  });
+
+  app.displayCityData = function(parsedCityData) {
+    // jQuery happens here
+
+    app.parseCityData();
+  };
 };
 
-app.displayCityData = function(parsedCityData) {
-	// jQuery happens here
-
-	app.parseCityData();
+app.convertUserInput = inputValue => {
+  inputValue
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .join('-');
 };
-
 app.init = function() {
-	$('#submit').on('click', async function(e) {
-		e.preventDefault();
-		//todo: function
-		app.userCity1 = $('#location1')
-			.val()
-			.trim()
-			.toLowerCase()
-			.split(' ')
-			.join('-');
-		app.userCity2 = $('#location2')
-			.val()
-			.trim()
-			.toLowerCase()
-			.split(' ')
-			.join('-');
+  $('#submit').on('click', async function(e) {
+    e.preventDefault();
+    //todo: function
+    app.userCity1 = $('#location1')
+      .val()
+      .trim()
+      .toLowerCase()
+      .split(' ')
+      .join('-');
+    app.userCity2 = $('#location2')
+      .val()
+      .trim()
+      .toLowerCase()
+      .split(' ')
+      .join('-');
 
-		app.getCityData(app.userCity1);
-		app.getCityData(app.userCity2);
-	});
+    app.getCityData(app.userCity1);
+    app.getCityData(app.userCity2);
+  });
 };
 
 //document ready
 $(document).ready(function() {
-	app.init();
+  app.init();
 });
 // end of document ready
